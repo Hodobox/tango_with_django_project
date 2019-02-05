ef13ff96fa55ffdd4de9a29a77da480baf94d23d
Samuel Gurský <samkogursky@gmail.com>
2019-02-05 10:22:26 +0100

    chapter 5 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -33,13 +33,13 @@ def populate():
     ]
 
     cats = {
-        "Python" : {"pages" : python_pages},
-        "Django" : {"pages" : django_pages},
-        "Other Frameworks" : {"pages" : other_pages}
+        "Python" : {"pages" : python_pages, "views" : 128, "likes" : 64},
+        "Django" : {"pages" : django_pages, "views" : 64, "likes" : 32},
+        "Other Frameworks" : {"pages" : other_pages, "views" : 32, "likes" : 16}
     }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cats[cat].get("views",0), cats[cat].get("likes",0))
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -56,8 +56,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views = 0, likes = 0):
+    c = Category.objects.get_or_create(name=name, views = views, likes = likes)[0]
     c.save()
     return c
      
